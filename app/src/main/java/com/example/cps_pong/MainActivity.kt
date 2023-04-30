2f5636d2357f6b4d050ed348d43d618c9c29e8fe
SorooshHamedi <hamedisoroosh@gmail.com>
2023-04-30 23:43:09 +0430

    initial commit

---
+++ app/src/main/java/com/example/cps_pong/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.cps_pong
+
+import android.graphics.Canvas
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.cps_pong.ui.theme.CPS_PongTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout)
+        setContent {
+            CPS_PongTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+    Canvas()
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CPS_PongTheme {
+        Greeting("Android")
+    }
+}
