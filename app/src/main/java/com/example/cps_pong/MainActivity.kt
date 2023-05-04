506f06df9f44ddcc6cbdf8035cfcda27849b524b
SorooshHamedi <hamedisoroosh@gmail.com>
2023-05-05 00:34:18 +0430

    started Canvas, draws the racket on a black background

--- app/src/main/java/com/example/cps_pong/MainActivity.kt
+++ app/src/main/java/com/example/cps_pong/MainActivity.kt
@@ -2,32 +2,31 @@ package com.example.cps_pong
 
 import android.graphics.Canvas
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cps_pong.ui.theme.CPS_PongTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var linearLayout: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.layout)
-        setContent {
-            CPS_PongTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        linearLayout = findViewById(R.id.idRLView)
+        val customView = MyCustomView(this)
+        linearLayout.addView(customView)
+//        setContent {
+//            CPS_PongTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colorScheme.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
+//        }
     }
 }
 
@@ -40,10 +39,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Canvas()
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CPS_PongTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    CPS_PongTheme {
+//        Greeting("Android")
+//    }
+//}
